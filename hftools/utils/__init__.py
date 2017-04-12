8bebf7951122b8a5d74aead557b51ed2a3719594
lukasheinrich <lukas.heinrich@gmail.com>
2017-04-12 13:34:30 +0200

    improvements

--- hftools/utils/__init__.py
+++ hftools/utils/__init__.py
@@ -22,153 +22,152 @@ def set_pars(ws,parpoint,reference_snapshot):
     ws.var(name).setVal(val)
 
 def isComponentFunc(channel,name,component = None):
-  if component:
-    return 'L_x_{}_{}'.format(component,channel) in name
-  else:
-    return name.startswith('L_x') and name.split('_')[3] == channel
+    if component:
+        return 'L_x_{}_{}'.format(component,channel) in name
+    else:
+        return name.startswith('L_x') and name.split('_')[3] == channel
 
 def shapeGaussVarNames(sysname,binnr):
-  gamma =  'gamma_{}_bin_{}'.format(sysname,binnr)
-  sigma =  'gamma_{}_bin_{}_sigma'.format(sysname,binnr)
-  return gamma,sigma
+    gamma =  'gamma_{}_bin_{}'.format(sysname,binnr)
+    sigma =  'gamma_{}_bin_{}_sigma'.format(sysname,binnr)
+    return gamma,sigma
 
 def shapePoissonVarNames(sysname,binnr):
-  gamma =  'gamma_{}_bin_{}'.format(sysname,binnr)
-  tau =  'gamma_{}_bin_{}_tau'.format(sysname,binnr)
-  return gamma,tau
+    gamma =  'gamma_{}_bin_{}'.format(sysname,binnr)
+    tau =  'gamma_{}_bin_{}_tau'.format(sysname,binnr)
+    return gamma,tau
 
 def samples(ws,channel):
-  allfuncs = ws.allFunctions()
-  it = allfuncs.iterator()
-  v = it.Next()
-  samples = []
-  while v:
-    name =  v.GetName()
-    if isComponentFunc(channel,name):
-      samples += [name.split('_')[2]]
+    allfuncs = ws.allFunctions()
+    it = allfuncs.iterator()
     v = it.Next()
-  return samples
+    samples = []
+    while v:
+        name =  v.GetName()
+        if isComponentFunc(channel,name):
+            samples += [name.split('_')[2]]
+        v = it.Next()
+    return samples
 
 def binwidth(ws,obs,channel,component):
-  for i,s in enumerate(samples(ws,channel)):
-    if s==component:
-      return ws.var(binwidthname(obs,channel,i)).getVal()
+    for i,s in enumerate(samples(ws,channel)):
+        if s==component:
+            return ws.var(binwidthname(obs,channel,i)).getVal()
 
 def extract_total(ws,channel,obs):
-  oname=obsname(obs,channel)
-  totalpdf = ws.pdf(totalpdfname(channel))
-  h = totalpdf.createHistogram(oname)
-  h.Scale(1./ws.var(binwidthname(obs,channel,0)).getVal())
-  return h
+    oname=obsname(obs,channel)
+    totalpdf = ws.pdf(totalpdfname(channel))
+    h = totalpdf.createHistogram(oname)
+    h.Scale(1./ws.var(binwidthname(obs,channel,0)).getVal())
+    return h
 
 def extract(ws,channel,obs,component = None):
-  if not component:
-    return extract_total(ws,channel,obs)
-  oname=obsname(obs,channel)
+    if not component:
+        return extract_total(ws,channel,obs)
+    oname=obsname(obs,channel)
 
-  allfuncs = ws.allFunctions()
-  it = allfuncs.iterator()
+    allfuncs = ws.allFunctions()
+    it = allfuncs.iterator()
 
-  v = it.Next()
-  while v:
-    name =  v.GetName()
-    if isComponentFunc(channel,name,component):
-      break
     v = it.Next()
+    while v:
+        name =  v.GetName()
+        if isComponentFunc(channel,name,component):
+            break
+        v = it.Next()
 
-  histo = v.createHistogram(oname)
-  histo.SetDirectory(0)
-  histo.Scale(binwidth(ws,obs,channel,component))
-  return histo
+    histo = v.createHistogram(oname)
+    histo.SetDirectory(0)
+    histo.Scale(binwidth(ws,obs,channel,component))
+    return histo
 
 def extract_with_pars(ws,channel,observable,component,pars,reference_snapshot = "NominalParamValues"):
-  set_pars(ws,pars,reference_snapshot)
-  return extract(ws,channel,observable,component)
+    set_pars(ws,pars,reference_snapshot)
+    return extract(ws,channel,observable,component)
 
 
 def extract_data(ws,channel,observable,name = None):
-  data = ws.data(dataName())
-  reduced = data.reduce('channelCat == channelCat::{}'.format(channel))
+    data = ws.data(dataName())
+    reduced = data.reduce('channelCat == channelCat::{}'.format(channel))
 
-  obsvar = ws.var(obsname(observable,channel))
+    obsvar = ws.var(obsname(observable,channel))
 
-  varlist = ROOT.RooArgList()
-  varlist.add(obsvar)
+    varlist = ROOT.RooArgList()
+    varlist.add(obsvar)
 
-  datahist = obsvar.createHistogram(name if name else 'data_{}'.format(channel))
-  datahist = reduced.fillHistogram(datahist,varlist)
-  datahist.Sumw2(0)
-  return datahist
+    datahist = obsvar.createHistogram(name if name else 'data_{}'.format(channel))
+    datahist = reduced.fillHistogram(datahist,varlist)
+    datahist.Sumw2(0)
+    return datahist
 
 def get_shapesys_pars(ws,observable,sysname,constraint_type):
-  allpars = pardict_up, pardict_nom, pardict_dn = {}, {}, {}
-  for binnr in range(observable.getBinning().numBins()):
-    gamma_name = 'gamma_{}_bin_{}'.format(sysname,binnr)
+    allpars = pardict_up, pardict_nom, pardict_dn = {}, {}, {}
+    for binnr in range(observable.getBinning().numBins()):
+        gamma_name = 'gamma_{}_bin_{}'.format(sysname,binnr)
 
-    mean_val,sigma_val = 0,0
-    if constraint_type == 'Gaussian':
-      gamma_name, gamma_sigma_name = shapeGaussVarNames(sysname,binnr)
-      sigma_val = ws.obj(gamma_sigma_name).getVal()
-      mean_val = ws.var(gamma_name).getVal()
+        mean_val,sigma_val = 0,0
+        if constraint_type == 'Gaussian':
+            gamma_name, gamma_sigma_name = shapeGaussVarNames(sysname,binnr)
+            sigma_val = ws.obj(gamma_sigma_name).getVal()
+            mean_val = ws.var(gamma_name).getVal()
 
-    if constraint_type == 'Poisson':
-      gamma_name, gamma_tau_name = shapePoissonVarNames(sysname,binnr)
-      mean_val = ws.var(gamma_name).getVal()
-      sigma_val = 1./math.sqrt((ws.function(gamma_tau_name).getVal()))
+        if constraint_type == 'Poisson':
+            gamma_name, gamma_tau_name = shapePoissonVarNames(sysname,binnr)
+            mean_val = ws.var(gamma_name).getVal()
+            sigma_val = 1./math.sqrt((ws.function(gamma_tau_name).getVal()))
 
-    pardict_up.update(**{gamma_name:mean_val+sigma_val})
-    pardict_nom.update(**{gamma_name:mean_val})
-    pardict_dn.update(**{gamma_name:mean_val-sigma_val})
-  return allpars
+        pardict_up.update(**{gamma_name:mean_val+sigma_val})
+        pardict_nom.update(**{gamma_name:mean_val})
+        pardict_dn.update(**{gamma_name:mean_val-sigma_val})
+    return allpars
 
 def getsys_pars(sysname,systype,**kwargs):
-  if systype in ['OverallSys','HistoSys']:
-    constraintvar = 'alpha_{}'.format(sysname)
-    return [{constraintvar:value} for value in [1.0,0.0,-1.0]]
-  if systype in ['ShapeSys']:
-    workspace      = kwargs['workspace']
-    observable     = kwargs['observable']
-    constraint_type = kwargs['constraint_type']
-    return get_shapesys_pars(workspace,observable,sysname,constraint_type)
-  if systype in ['Lumi']:
-    workspace      = kwargs['workspace']
-    return [{'Lumi':v} for v in getParFromConstraint(workspace,'lumiConstraint','Lumi')]
+    if systype in ['OverallSys','HistoSys']:
+        constraintvar = 'alpha_{}'.format(sysname)
+        return [{constraintvar:value} for value in [1.0,0.0,-1.0]]
+    if systype in ['ShapeSys']:
+        workspace      = kwargs['workspace']
+        observable     = kwargs['observable']
+        constraint_type = kwargs['constraint_type']
+        return get_shapesys_pars(workspace,observable,sysname,constraint_type)
+    if systype in ['Lumi']:
+        workspace      = kwargs['workspace']
+        return [{'Lumi':v} for v in getParFromConstraint(workspace,'lumiConstraint','Lumi')]
 
 def getParFromConstraint(ws,constraintname,var):
-  # import scipy.stats
-  # pvals = [scipy.stats.norm.cdf(x) for x in [1,0,-1]]
-  # no need for a scipy dependence for just three numbers
-  # for +1,0,-1 sigma pvalues
-  pvals = [0.84134474606854293, 0.5, 0.15865525393145707]
+    # import scipy.stats
+    # pvals = [scipy.stats.norm.cdf(x) for x in [1,0,-1]]
+    # no need for a scipy dependence for just three numbers
+    # for +1,0,-1 sigma pvalues
+    pvals = [0.84134474606854293, 0.5, 0.15865525393145707]
 
-  varobj = ws.var(var)
-  argset = ROOT.RooArgSet()
-  argset.add(varobj)
-  constraint = ws.pdf(constraintname)
-  cdf = constraint.createCdf(argset)
+    varobj = ws.var(var)
+    argset = ROOT.RooArgSet()
+    argset.add(varobj)
+    constraint = ws.pdf(constraintname)
+    cdf = constraint.createCdf(argset)
 
-  x = up,nom,dn = [cdf.findRoot(varobj,varobj.getMin(),varobj.getMax(),pval) for pval in pvals]
+    x = up,nom,dn = [cdf.findRoot(varobj,varobj.getMin(),varobj.getMax(),pval) for pval in pvals]
 
-  return x
+    return x
 
 def make_band_root(up,down,nominal,binmin = 0,binmax = 1):
-  g = ROOT.TGraphAsymmErrors(nominal.GetNbinsX())
-  for i in range(1,nominal.GetNbinsX()+1):
-    x_nom = nominal.GetBinCenter(i)
-    x_lo = nominal.GetBinLowEdge(i)
-    x_hi = nominal.GetBinLowEdge(i)+nominal.GetBinWidth(i)\
+    g = ROOT.TGraphAsymmErrors(nominal.GetNbinsX())
+    for i in range(1,nominal.GetNbinsX()+1):
+        x_nom = nominal.GetBinCenter(i)
+        x_lo = nominal.GetBinLowEdge(i)
+        x_hi = nominal.GetBinLowEdge(i)+nominal.GetBinWidth(i)\
 
-    y_nom,y_up,y_down = [x.GetBinContent(i) for x in [nominal,up,down]]
+        y_nom,y_up,y_down = [x.GetBinContent(i) for x in [nominal,up,down]]
 
-    binwidth = x_hi-x_lo
-    center = x_lo + binwidth*(binmax-binmin)/2.0
-    left   = x_lo + binwidth*binmin
-    right  = x_lo + binwidth*binmax
+        binwidth = x_hi-x_lo
+        center = x_lo + binwidth*(binmax-binmin)/2.0
+        left   = x_lo + binwidth*binmin
+        right  = x_lo + binwidth*binmax
 
-    g.SetPoint(i-1,center,y_nom)
-    g.SetPointEXhigh(i-1,right-center)
-    g.SetPointEXlow(i-1,center-left)
-    g.SetPointEYhigh(i-1,y_up-y_nom)
-    g.SetPointEYlow(i-1,y_nom-y_down)
-
-  return g
+        g.SetPoint(i-1,center,y_nom)
+        g.SetPointEXhigh(i-1,right-center)
+        g.SetPointEXlow(i-1,center-left)
+        g.SetPointEYhigh(i-1,y_up-y_nom)
+        g.SetPointEYlow(i-1,y_nom-y_down)
+    return g
--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from setuptools import setup,find_packages
 
 setup(
-  name = 'hftools',
-  version = '0.0.1',
-  packages = find_packages(),
-  author = 'Lukas Heinrich',
-  author_email = 'lukas.heinrich@cern.ch',
-  description = 'simple tools to extract to do post-processing on HistFactory configurations / workspaces (e.g. extract data for HepData, simple stacked plots etc.)',
-  install_requires = [
-    'click',
-    'pyyaml',
-    'numpy',
-  ],
+    name = 'hftools',
+    version = '0.0.1',
+    packages = find_packages(),
+    author = 'Lukas Heinrich',
+    author_email = 'lukas.heinrich@cern.ch',
+    description = 'simple tools to extract to do post-processing on HistFactory configurations / workspaces (e.g. extract data for HepData, simple stacked plots etc.)',
+    install_requires = [
+        'click',
+        'pyyaml',
+        'numpy',
+        'brewer2mpl',
+    ],
+    entry_points = {
+        'console_scripts': [
+            'hfquickplot = hftools.plotting.quickplot:toplevel',
+      ]
+    },
 )
