0cd376bfd200ee8e91084839a635f2a5bb9d71db
lukasheinrich <lukas.heinrich@gmail.com>
2017-06-16 13:39:47 +0200

    Bump version: 0.0.3 → 0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name = 'hftools',
-    version = '0.0.3',
+    version = '0.0.4',
     packages = find_packages(),
     author = 'Lukas Heinrich',
     author_email = 'lukas.heinrich@cern.ch',
